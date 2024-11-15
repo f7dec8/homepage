747660e73312718725d435aac83af604fd84a383
f7dec8 <f7dec8@gmail.com>
2024-11-15 23:35:07 +0900

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,6 +19,7 @@ export default defineNuxtConfig({
 
   app: {
     baseURL: '/bns-neo/',
+    // buildAssetsDir: 'assets',
     head: {
       viewport: 'width=device-width,initial-scale=1',
       link: [
